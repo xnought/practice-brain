0c655330653d8d16100eb02687bddcfc400f8da2
xnought <drbertucci@hotmail.com>
2025-04-04 20:55:59 -0400

    bet2 skull strupping

--- andy/unix/if.sh
+++ andy/unix/if.sh
@@ -10,4 +10,24 @@ else
 	echo "fsl needs to be downloaded"
 fi
 
-                      
+cd ../ds000102/sub-01/anat
+if [[ -e sub-01_T1w_brain_f02.nii.gz ]]; then
+	echo “Skull-stripped brain exists”
+else
+	echo “Skull-stripped brain does not exist”
+fi
+
+cd ../../ # back to ds000102/ root
+for i in `seq -w 1 26`; do
+	sub="sub-$i"
+	cd $sub/anat
+	
+	if [[ ! -e ${sub}_T1w_f02_brain.nii.gz ]]; then
+			echo “Skull-stripped brain doesn’t exist stripping the brain with a fractional intensity threshold of 0.2”
+			bet2 ${sub}_T1w.nii.gz ${sub}_T1w_f02_brain.nii.gz -f 0.2
+	else
+			echo “Skull-Stripped brain already exists doing nothing”
+	fi
+	
+	cd ../../
+done
